b8e97ab7422cc51c6eca1cea8635338b42cf9600
Dan Quimby <daqu40@outlook.com>
2024-02-02 09:26:43 -0700

    Initial commit

---
+++ ChangeVDeskTopLeft.ps1
@@ -0,0 +1,24 @@
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class Keyboard {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+    }
+"@
+
+# Define the key codes
+$VK_CONTROL = 0x11
+$VK_LWIN = 0x5B
+$VK_LEFT = 0x25
+
+# Press the keys
+[Keyboard]::keybd_event($VK_CONTROL, 0, 0, 0)
+[Keyboard]::keybd_event($VK_LWIN, 0, 0, 0)
+[Keyboard]::keybd_event($VK_LEFT, 0, 0, 0)
+
+# Release the keys
+[Keyboard]::keybd_event($VK_LEFT, 0, 2, 0)
+[Keyboard]::keybd_event($VK_LWIN, 0, 2, 0)
+[Keyboard]::keybd_event($VK_CONTROL, 0, 2, 0)
---
+++ README.md
@@ -0,0 +1,5 @@
+# PowerShell scripts for Changing Windows virtual desktops by clicking in the taskbar.
+
+Since I use a lot of Remote systems that have virtual desktops, I need a way to easily change Virtual desktops on the remote system without changing the one on the local system.
+
+These scripts and shortcut allow you to put shortcuts in the taskbar and click to change Virtual desktops.
